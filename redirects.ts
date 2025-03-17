473242a2c56ed9192be9d72959b2ae6b8aab03f9
kingsonseang <kingsonseang@gmail.com>
2025-03-17 15:20:02 +0100

    Add redirect for pitch deck to Google Drive link in redirects.ts

--- redirects.ts
+++ redirects.ts
@@ -19,6 +19,12 @@ const redirects = async () => {
       destination: "https://youtu.be/QPx52UNhkRc",
       permanent: true,
     },
+    {
+      source: "/pitchdeck",
+      destination:
+        "https://drive.google.com/file/d/1Bu6_ndqZo21-2L9TNNgs5IhnT4dSTWX7/view?usp=drivesdk",
+      permanent: true,
+    },
   ];
 
   return redirects;
